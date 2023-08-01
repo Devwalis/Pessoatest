3c36416264abbb9c0384009fe0eb5fe3db1fb751
Walisson Oliveira <walissonoliveira1@outlook.com>
2023-08-01 18:25:26 -0300

    Projeto java

---
+++ src/main/java/apirest/restapi/RestapiApplication.java
@@ -0,0 +1,19 @@
+package apirest.restapi;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.fasterxml.jackson.core.sym.Name1;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiApplication.class, args);
+
+	
+	}
+
+}
