05475d6bc57916ae54f9e0966e593399fcf0e447
Walisson Oliveira <walissonoliveira1@outlook.com>
2023-08-06 01:57:31 -0300

    TipoUsuario

---
+++ src/main/java/apirest/restapi/Adm.java
@@ -0,0 +1,22 @@
+package restapi apirest;
+
+Class Adm{
+
+ int id;
+ String nome;
+ int idade;
+ String email;
+
+ Public Adm(){
+ }
+ puclic Adm( int id, String nome, String email;)
+ super();
+this.id = id;
+this.nome = nome;
+this.idade = idade;
+this.email = email;
+
+
+
+
+}
